c82cec6c021779dc7836d360d5a02feaf32bf668
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2022-02-06 11:44:08 -0800

    workflow with dependencies.

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 import logging
 import os
+from pythonjsonlogger import jsonlogger
 
 LOGGER = logging.getLogger()
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
 API_KEY = os.getenv('API_KEY', 'SUPER_SECRET_KEY')
 LOGGER.setLevel(LOG_LEVEL)
 handler = logging.StreamHandler()
-formatter = logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(lineno)d - %(message)s"
-)
+formatter = jsonlogger.JsonFormatter()
 handler.setFormatter(formatter)
 LOGGER.addHandler(handler)
 LOGGER = logging.getLogger(__name__)
