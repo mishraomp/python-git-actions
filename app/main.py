1e53d70b83b3c5c5b1546bcd6ed8c06e76159963
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2022-02-06 11:44:42 -0800

    workflow with dependencies.

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from pythonjsonlogger import jsonlogger
 
 LOGGER = logging.getLogger()
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
-API_KEY = os.getenv('API_KEY', 'SUPER_SECRET_KEY')
+API_SECRET_KEY = os.getenv('API_KEY', 'SUPER_SECRET_KEY')
 LOGGER.setLevel(LOG_LEVEL)
 handler = logging.StreamHandler()
 formatter = jsonlogger.JsonFormatter()
@@ -13,4 +13,4 @@ LOGGER.addHandler(handler)
 LOGGER = logging.getLogger(__name__)
 
 LOGGER.info(f"This is Log Level '{LOG_LEVEL}'")
-LOGGER.info(f"This is API KEY '{API_KEY}'")
+LOGGER.info(f"This is API KEY '{API_SECRET_KEY}'")
